30192bebd395df6d8270bd382df1808c80b053f0
CyberGutz <gutemagae@gmail.com>
2022-10-29 09:55:57 -0300

    Reestruturação e limpeza de código

--- ClassesAFD/Util.py
+++ ClassesAFD/Util.py
@@ -2,12 +2,8 @@ def char_remove(old, to_remove):
         nova_string = old
         for x in to_remove:
             nova_string = nova_string.replace(x, '')
-
         return nova_string
 
 
-def copiaAfd(afd):
-        copia = afd;
-        return copia;
 
     
